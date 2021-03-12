afd718688582822537fbc8c588a55b9a67319e8b
Shreejan <dolaishreejan@gmail.com>
2021-03-12 10:52:25 +0530

    Initial commit

---
+++ area_of_rectangle.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+
+int main(){
+    int l;
+    int b;
+    
+   
+    printf("Give the length : ");
+    scanf("%d", &l);
+    printf("Give the breadth : ");
+    scanf("%d", &b);
+    printf("The area is %d", l * b);
+    
+    return 0;
+}
